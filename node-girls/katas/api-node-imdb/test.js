b745563829f7fc112a96760b7d268531f232caf3
Yan <oodruby@hotmail.com>
2018-10-17 07:45:24 +1100

    added new test to test empty description field in createMovie function

--- node-girls/katas/api-node-imdb/test.js
+++ node-girls/katas/api-node-imdb/test.js
@@ -1,13 +1,23 @@
 function createMovie(title, description){
-    return "Error: Missing movie title"
+    if(!title){
+        return "Error: Missing movie title"
+    }
+    return "Error: Missing movie description"
 }
 //Here be the tests
 describe('Creating a move', () => {
-    it('Should throw and error at an empty title', () => {
+    it('Should throw an error at an empty title', () => {
         const movie = {
             title: '',
             description: 'hello world'
         }
         expect(createMovie(movie)).toEqual('Error: Missing movie title');
         })
+    it('Should throw an error at an empty description', () => {
+        const movie = {
+            title: 'hello world',
+            description: ''
+        }
+        expect(createMovie(movie)).toEqual('Error: Missing movie description')
+    })
 })
