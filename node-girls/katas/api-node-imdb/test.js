7aa165b946b424e28fb3c9d2a7a0887b21583719
Yan <oodruby@hotmail.com>
2018-10-17 07:57:24 +1100

    new test to test the output and ensure that createMovie returns the right JSON

--- node-girls/katas/api-node-imdb/test.js
+++ node-girls/katas/api-node-imdb/test.js
@@ -1,11 +1,20 @@
-function createMovie(title, description){
-    if(!title){
+function createMovie(movie){
+    if(!movie.title){
         return "Error: Missing movie title"
+    } else if (!movie.description){
+        return "Error: Missing movie description"
     }
-    return "Error: Missing movie description"
+    return {
+        movie,
+        status: 'successfully added movie'
+    }.toJSON;
 }
+
+
 //Here be the tests
 describe('Creating a move', () => {
+
+    //The movie must have BOTH a title and description
     it('Should throw an error at an empty title', () => {
         const movie = {
             title: '',
@@ -13,6 +22,7 @@ describe('Creating a move', () => {
         }
         expect(createMovie(movie)).toEqual('Error: Missing movie title');
         })
+        
     it('Should throw an error at an empty description', () => {
         const movie = {
             title: 'hello world',
@@ -20,4 +30,27 @@ describe('Creating a move', () => {
         }
         expect(createMovie(movie)).toEqual('Error: Missing movie description')
     })
+
+    /*User must send the data to the API in the following JSON format:
+     output: {
+        movie: {
+            id,
+            title,
+            description
+        },
+        status: 'successfully added movie'
+    }*/
+    it('Should send data to the API in a JSON format', () => {
+        const movie = {
+            title: "hello darkness,",
+            description: "my old friend"
+        }
+        const expectedResult = {
+            movie,
+            status: 'successfully added movie'
+        }
+        expect(createMovie(movie)).toEqual(expectedResult.toJSON);
+    })
+    
+    //User cannot enter duplicate movies (same title)
 })
